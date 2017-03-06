5465a60aea351a4118698fe96b3077a6a4424647
Ondřej Švec <ond.svec@gmail.com>
2017-03-07 00:24:55 +0100

    Removed best algrithms

--- techniques.py
+++ techniques.py
@@ -5,9 +5,6 @@ from sklearn.preprocessing import Imputer, StandardScaler, LabelEncoder
 from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
 
-from metric_learn import LMNN, NCA, LFDA, Covariance, MetricEvolution, NeuralNetworkTransformer, FullMatrixTransformer, DiagonalMatrixTransformer
-from metric_learn import ITML_Supervised, SDML_Supervised, LSML_Supervised, RCA_Supervised
-
 sortedTechniques = [
     ('kNN', 'Euclidean'),
     ('stand+kNN', 's:Euclidean'),
@@ -50,25 +47,6 @@ sortedTechniques = [
     
 #     ('SDML+kNN', 'SDML'),
 #     ('stand+SDML+kNN', 's:SDML'),
-]
-
-common_ea_params = {
-    's__stats': None,
-    's__n_gen': 200,
-    'transformer': 'full',
-}
-
-bestAlgorithms = [
-    ('stand+Cov+kNN', Covariance()),
-    ('stand+LMNN+kNN', LMNN(k=4, regularization=0.9, learn_rate=1e-8, max_iter=1000)),
-    ('stand+NCA+kNN', NCA(max_iter=1000, learning_rate=0.01)),
-    ('stand+LFDA+kNN', LFDA(k=3, dim=None)),
-    
-    ('stand+CMAES+kNN', MetricEvolution(**common_ea_params, strategy='cmaes', fitnesses=[('knn', {'n_neighbors':8})])),
-    ('stand+CMAESFme+kNN', MetricEvolution(**common_ea_params, strategy='cmaes', fitnesses='wfme')),
-    
-    ('stand+JDE+kNN', MetricEvolution(**common_ea_params, strategy='jde', fitnesses='wfme')),
-    ('stand+JDEkNN+kNN', MetricEvolution(**common_ea_params, strategy='jde', fitnesses=[('knn', {'n_neighbors':8})])),
 ]
 
 sortedDatasets = [
